3a98cd755ecba341e63f5cc8f30ba517ca8b9148
jghaanstra <jghaanstra@gmail.com>
2019-08-11 14:36:12 +0200

    Release 1.2.0

--- README.md
+++ README.md
@@ -36,5 +36,5 @@ After that you can add your Synology Surveillance Center as device within Homey 
 To be able to send snapshots through email you will need to configure an email account which sends out the email. In the general settings of the Surveillance Station App there is a section to configure your email account. Please pay attention to the extra information when adding a Gmail account, this requires you to use a specific app password which needs to be setup within your Google account.
 
 ## Changelog
-### v1.1.2 - 2019-06-27
-* NEW: added action card for setting motion detection
+### v1.2.0 - 2019-08-11
+* IMPROVEMENT: added device discovery
--- drivers/surveillancestation/driver.js
+++ drivers/surveillancestation/driver.js
@@ -6,16 +6,59 @@ const util = require('/lib/util.js');
 class SurveillanceStationDriver extends Homey.Driver {
 
   onPair(socket) {
-    socket.on('testConnection', function(data, callback) {
-      let path = 'http://'+ data.address +':'+ data.port +'/webapi/query.cgi?api=SYNO.API.Info&method=Query&version=1&query=SYNO.API.Auth,SYNO.SurveillanceStation';
+    const discoveryStrategy = this.getDiscoveryStrategy();
+    const discoveryResults = discoveryStrategy.getDiscoveryResults();
+    let selectedDeviceId;
+    let deviceArray = {};
+
+    socket.on('list_devices', (data, callback) => {
+      const devices = Object.values(discoveryResults).map(discoveryResult => {
+        return {
+          name: 'Surveillance Station ['+ discoveryResult.address +']',
+          data: {
+            id: discoveryResult.id,
+          }
+        };
+      });
+      callback(null, devices);
+    });
+
+    socket.on('list_devices_selection', (data, callback) => {
+      callback();
+      selectedDeviceId = data[0].data.id;
+    });
+
+    socket.on('login', (data, callback) => {
+      const discoveryResult = discoveryResults[selectedDeviceId];
+      if(!discoveryResult) return callback(new Error('Something went wrong'));
+
+      let path = 'http://'+ discoveryResult.address +':'+ Number(discoveryResult.txt.admin_port) +'/webapi/query.cgi?api=SYNO.API.Info&method=Query&version=1&query=SYNO.API.Auth,SYNO.SurveillanceStation';
       util.sendCommand(path)
         .then(result => {
+          deviceArray = {
+            name: 'Surveillance Station ['+ discoveryResult.address +']',
+            data: {
+              id: discoveryResult.id,
+            },
+            settings: {
+              address: discoveryResult.address,
+              port: Number(discoveryResult.txt.admin_port),
+      				username: data.username,
+      				password: data.password
+            }
+          }
           callback(false, result);
         })
         .catch(error => {
           callback(error, false);
         })
+
     });
+
+    socket.on('get_device', (data, callback) => {
+      callback(false, deviceArray);
+    });
+
   }
 
 }
